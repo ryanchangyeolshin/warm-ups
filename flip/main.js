72471c5ed37babe534f4e90953f6117a6d1abd08
Ryan Shin <changyeolshin@gmail.com>
2017-09-06 15:45:10 -0700

    Added index.html and main.js for the flip exercise.

---
+++ flip/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+var randomArray = [1, 'Jamie', 'Logan', 3, true]
+
+function flip(array) {
+  var newArray = []
+  for (var i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i])
+  }
+  return newArray
+}
