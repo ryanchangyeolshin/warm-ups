890142428b4658649213fb1922983dd713aa8067
Ryan Shin <changyeolshin@gmail.com>
2017-09-10 15:28:33 -0700

    Modified the simple list app to delete certain lists.

--- javascript-dom-events/main.js
+++ javascript-dom-events/main.js
@@ -1,41 +1,63 @@
 /* eslint-disable no-unused-vars */
 var $list = document.querySelector('.list')
 var $text = document.querySelector('#text')
-var $button = document.querySelector('#submit-button')
+var $submitButton = document.querySelector('#submit-button')
+var $deleteButton = null;
+var idAttribute = null;
+
+// This variable represents the count index of the list items
+var listIndex = 1;
 
 function renderListItems() {
   // Create the list element
   var $listItem = document.createElement('li')
 
   // Create the delete button element
-  var $deleteButton = document.createElement('button')
+  $deleteButton = document.createElement('button')
 
-  // Give the created list element a class attribute
-  $listItem.setAttribute('id', 'list-items')
+  // Give the created list element a class attribute and push it to the array
+  $listItem.setAttribute('class', 'list-items')
+  // Also give the $listItem an id attribute associated with the index
+  $listItem.setAttribute('id', 'list' + listIndex)
 
   // Give the delete button an id attribute
-  $deleteButton.setAttribute('id', 'delete-button')
+  $deleteButton.setAttribute('class', 'delete-button')
+  $deleteButton.setAttribute('id', listIndex)
+
+  // Then increment the listIndex by one
+  listIndex++
 
   // Make the text content
   $listItem.textContent = $text.value
   $deleteButton.textContent = 'X'
 
-  // Append it as the child of the <ul class="list"> tag
-  $list.appendChild($listItem)
-
   // Append the delete button to the <li> tag
   $listItem.appendChild($deleteButton)
 
-  // Provide a setTimeout alert
-  setTimeout(function() {
-    alert('You typed in a list!')
-  }, 1000)
+  return $listItem
 }
 
 function deleteList(e) {
-  var $listItem = e.target;
-  console.log(e.target)
+  idAttribute = '#list' + e.target.getAttribute('id')
+  console.log(idAttribute)
+  var $listItem = document.querySelector(idAttribute)
+
+  $list.removeChild($listItem)
 }
 
-$button.addEventListener('click', renderListItems)
-$deleteButton.addEventListener('click', deleteList)
+$submitButton.addEventListener('click', function() {
+  $list.appendChild(renderListItems())
+
+  // Make the input box empty once clicked submit
+  $text.value = ''
+
+  // Reassign the delete button
+  $deleteButton = document.querySelectorAll('.delete-button')
+
+  // Iterate through the delete buttons
+  for (var i = 0; i < $deleteButton.length; i++) {
+    $deleteButton[i].addEventListener('click', function (e) {
+      deleteList(e)
+    })
+  }
+})
