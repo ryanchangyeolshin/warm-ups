fc68bd5735c64a255dbeb5338e140a82cc071d6e
Ryan Shin <changyeolshin@gmail.com>
2017-09-13 14:19:19 -0700

    Modified the main.js code from the event-delegation exercise.

--- event-delegation/main.js
+++ event-delegation/main.js
@@ -1,8 +1,6 @@
 var $pokedex = document.querySelector('#pokedex')
 
 $pokedex.addEventListener('click', function (e) {
-  var $pokemons = document.querySelectorAll('li')
-  for (var i = 0; i < $pokemons.length; i++) {
-    console.log($pokemons[i].getAttribute('data-number'))
-  }
-})
+  var $pokemonDataNumber = e.target.getAttribute('data-number')
+  console.log($pokemonDataNumber)
+}, false)
