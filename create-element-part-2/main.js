531e43fffaa2b4437afbfca15e922ffc2c18c3e1
Ryan Shin <changyeolshin@gmail.com>
2017-09-26 09:29:49 -0700

    Added files for the create-elements-part-2 exercise.

---
+++ create-element-part-2/main.js
@@ -0,0 +1,44 @@
+function createElement(tagName, attributes, children) {
+  const $HTMLElement = document.createElement(tagName)
+  for (var attribute in attributes) {
+    $HTMLElement.setAttribute(attribute, attributes[attribute])
+  }
+  for (var i = 0; i < children.length; i++) {
+    $HTMLElement.appendChild(children[i])
+  }
+  return $HTMLElement
+}
+
+const $article =
+  createElement('div', { 'data-id': 1234 }, [
+    createElement('h1', { class: 'title' }, []),
+    createElement('h4', { class: 'author' }, []),
+    createElement('p', { class: 'content' }, [])
+  ])
+
+console.log($article)
+
+/* $article
+ * <div data-id="1234">
+ *   <h1 class="title"></h1>
+ *   <h4 class="author"></h4>
+ *   <p class="content"></p>
+ * </div>
+ */
+
+const $list =
+  createElement('ul', { class: "list-group" }, [
+    createElement('li', { class: "list-group-item" }, []),
+    createElement('li', { class: "list-group-item" }, []),
+    createElement('li', { class: "list-group-item" }, [])
+  ])
+
+/* $list
+ * <ul class="list-group">
+ *   <li class="list-group-item"></li>
+ *   <li class="list-group-item"></li>
+ *   <li class="list-group-item"></li>
+ * </ul>
+ */
+
+ console.log($list)
