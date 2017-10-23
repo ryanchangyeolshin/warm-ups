18fd17d355715090e8cb205b83e774a4cb2b0023
Ryan Shin <changyeolshin@gmail.com>
2017-10-23 09:29:59 -0700

    Added files for the express-hello-world exercise.

---
+++ express-hello-world/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("Hello, World!")
+})
+
+app.listen(3000, err => {
+  if (err) {
+    console.error(err)
+  }
+  else {
+    console.log("Listening on PORT 3000")
+  }
+})
