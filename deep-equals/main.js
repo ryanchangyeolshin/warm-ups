de6c9d5d99c530af3838cd168d28e500ae09b0dc
Ryan Shin <changyeolshin@gmail.com>
2017-10-31 10:16:47 -0700

    Refactored code.

--- deep-equals/main.js
+++ deep-equals/main.js
@@ -1,30 +1,28 @@
-const deepEquals = (val1, val2) => {
-  return JSON.stringify(val1) === JSON.stringify(val2)
+function typeEquals(val1, val2) {
+  return Object.prototype.toString.call(val1) === Object.prototype.toString.call(val2)
 }
 
-const deepEquals2 = (val1, val2) => {
-  if (typeof(val1) === typeof(val2)) {
-    switch (typeof(val1)) {
-      case 'object':
-        if (Array.isArray(val1)) {
-          for (var i = 0; i < val1.length; i++) {
-            if (val1[i].toString() !== val2[i].toString()) {
-              return false
-            }
-          }
-          return true
-        }
-        else {
-          return true
-        }
-        break
-      default:
-        return val1 === val2
-    }
+function isObject(val) {
+  return Object.prototype.toString.call(val) === '[object Object]'
+}
+
+function isArray(val) {
+  return Array.isArray(val)
+}
+
+function deepEquals(val1, val2) {
+  if (!typeEquals(val1, val2)) return false
+  if (isArray(val1)) {
+    return val1.length === val2.length &&
+           val1.every((element, i) => deepEquals(element, val2[i]))
   }
-  else {
-    return false
+  if (isObject(val1)) {
+    const keys1 = Object.keys(val1)
+    const keys2 = Object.keys(val2)
+    return keys1.length === keys2.length &&
+           keys1.every(key => deepEquals(val1[key], val2[key]))
   }
+  return val1 === val2
 }
 
 console.log(deepEquals(42, 42)) // true
@@ -34,18 +32,6 @@ console.log(deepEquals([1, 2, 3], [1, 2, 3])) // true
 console.log(deepEquals({ foo: 'bar' }, { foo: 'bar' })) // true
 
 console.log(deepEquals(
-  [{ baz: 'qux' }, { corge: 'grault' }],
-  [{ baz: 'qux' }, { corge: 'grault' }]
-)) // true
-
-
-console.log(deepEquals2(42, 42)) // true
-
-console.log(deepEquals2([1, 2, 3], [1, 2, 3])) // true
-
-console.log(deepEquals2({ foo: 'bar' }, { foo: 'bar' })) // true
-
-console.log(deepEquals2(
   [{ baz: 'qux' }, { corge: 'grault' }],
   [{ baz: 'qux' }, { corge: 'grault' }]
 )) // true
