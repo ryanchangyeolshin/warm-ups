04c784246c2db9095899376fc886bb45676e87ce
Ryan Shin <changyeolshin@gmail.com>
2017-11-17 10:29:58 -0800

    Added another function to the ZipWith exercise.

--- ZipWith/main.js
+++ ZipWith/main.js
@@ -1,20 +1,26 @@
 const zipWith = (...lists) => {
   const iteratee = lists.pop()
   const zipped = []
-  for (var i = 0; i < lists[0].length; i++) {
+  for (let i = 0; i < lists[0].length; i++) {
     const filtered = []
     lists.forEach(list => filtered.push(list[i]))
     zipped.push(iteratee(...filtered))
   }
-  console.log(zipped)
+  return zipped
 }
 
-zipWith([1, 2], [10, 20], [100, 200], (x, y, z) => {
+const zipWith2 = (...lists) => {
+  const iteratee = lists.pop()
+  const [ longest ] = lists.slice().sort((a, b) => a.length < b.length)
+  longest.map((_, i) => iteratee(...lists.map(list => list[i])))
+}
+
+zipWith2([1, 2], [10, 20], [100, 200], (x, y, z) => {
   return x + y + z
 })
 // [111, 222]
 
-zipWith([1, 2], [10, 20], [100], (x, y, z) => {
+zipWith2([1, 2], [10, 20], [100], (x, y, z) => {
   return x + y + z
 })
 // [111, NaN]
